eeaca1ad9c8aeb3af1224f7841385bcb68093834
UltCombo <ultcombo@gmail.com>
2014-10-09 11:08:55 -0300

    Filtrar já contemplados

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var presentes = require('./presentes.json');
+'use strict';
+
+var fs = require('fs'),
+	presentes = require('./presentes.json'),
+	sorteados = require('./sorteados.json');
+
+presentes = presentes.filter(function(participante) {
+	return sorteados.indexOf(participante.id) === -1;
+});
+
+if (!presentes.length) {
+	console.log('Todos participantes presentes já foram sorteados.');
+	return;
+}
 
 var sorteado = presentes[ Math.floor(Math.random() * presentes.length) ];
 
 console.log( sorteado.id, sorteado.nome );
 
+sorteados.push(sorteado.id);
+fs.writeFileSync('sorteados.json', JSON.stringify(sorteados, null, '  '));
